44b511943ca7daa0f1ef066377a5383f4a7ce5ef
amir.daliri <amir.daliri@zoodfood.com>
2018-09-16 15:14:21 +0430

    Initial Commit

---
+++ CoreML-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML-Test
+//
+//  Created by Amir Daliri on 6/25/1397 AP.
+//  Copyright © 1397 AmirDaliri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
